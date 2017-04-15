4d3baf7e18b88f913ee48a65347dc077ce353694
Nobuyoshi Aquino <nobuyoshi.aquino@gmail.com>
2017-04-15 01:40:49 -0400

    Draw on map your current location and the nearest photo to you

--- app/src/main/java/xyz/nobu/locatr/GalleryItem.java
+++ app/src/main/java/xyz/nobu/locatr/GalleryItem.java
@@ -10,6 +10,8 @@ public class GalleryItem {
     private String mCaption;
     private String mId;
     private String mUrl;
+    private double mLat;
+    private double mLon;
     private String mOwner;
 
     @Override
@@ -55,5 +57,21 @@ public class GalleryItem {
                 .appendPath(mOwner)
                 .appendPath(mId)
                 .build();
+    }
+
+    public double getLat() {
+        return mLat;
+    }
+
+    public void setLat(double lat) {
+        mLat = lat;
+    }
+
+    public double getLon() {
+        return mLon;
+    }
+
+    public void setLon(double lon) {
+        mLon = lon;
     }
 }
--- app/src/main/java/xyz/nobu/locatr/LocatrFragment.java
+++ app/src/main/java/xyz/nobu/locatr/LocatrFragment.java
@@ -8,21 +8,26 @@ import android.location.Location;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.List;
@@ -31,17 +36,20 @@ import java.util.List;
  * Created by nobu on 4/13/17.
  */
 
-public class LocatrFragment extends Fragment {
+public class LocatrFragment extends SupportMapFragment {
 
     private static final String TAG = "LocatrFragment";
-    private static final String[] LOCATION_PERMISIONS = new String[] {
+    private static final String[] LOCATION_PERMISSIONS = new String[] {
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION
     };
     private static final int REQUEST_LOCATION_PERMISSIONS = 0;
 
-    private ImageView mImageView;
     private GoogleApiClient mClient;
+    private GoogleMap mMap;
+    private Bitmap mMapImage;
+    private GalleryItem mMapItem;
+    private Location mCurrentLocation;
 
     public static LocatrFragment newInstace() {
         return new LocatrFragment();
@@ -75,16 +83,14 @@ public class LocatrFragment extends Fragment {
                     }
                 })
                 .build();
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_locatr, container, false);
-
-        mImageView = (ImageView) v.findViewById(R.id.image);
-
-        return v;
+        getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
+                updateUI();
+            }
+        });
     }
 
     @Override
@@ -118,7 +124,7 @@ public class LocatrFragment extends Fragment {
                 if (hasLocationPermission()) {
                     findImage();
                 } else {
-                    requestPermissions(LOCATION_PERMISIONS,
+                    requestPermissions(LOCATION_PERMISSIONS,
                             REQUEST_LOCATION_PERMISSIONS);
                 }
                 return true;
@@ -187,16 +193,63 @@ public class LocatrFragment extends Fragment {
          * checkSelfPermission(...) instead to avoid ugly conditional code.
          */
         int result = ContextCompat
-                .checkSelfPermission(getActivity(), LOCATION_PERMISIONS[0]);
+                .checkSelfPermission(getActivity(), LOCATION_PERMISSIONS[0]);
         return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void updateUI() {
+        if (mMap == null || mMapImage ==null) {
+            return;
+        }
+
+        LatLng itemPoint = new LatLng(mMapItem.getLat(), mMapItem.getLon());
+        LatLng myPoint = new LatLng(
+                mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+
+        BitmapDescriptor itemBitmap = BitmapDescriptorFactory.fromBitmap(mMapImage);
+        MarkerOptions itemMarker = new MarkerOptions()
+                .position(itemPoint)
+                .icon(itemBitmap);
+        MarkerOptions myMarker = new MarkerOptions()
+                .position(myPoint);
+
+        mMap.clear();
+        mMap.addMarker(itemMarker);
+        mMap.addMarker(myMarker);
+
+        LatLngBounds bounds = new LatLngBounds.Builder()
+                .include(itemPoint)
+                .include(myPoint)
+                .build();
+
+        /**
+         * To move your GoogleMap around, you built a CameraUpdate.
+         * CameraUpdateFactory has a variety of static methods to build
+         * different kinds of CameraUpdate objects that adjust the position,
+         * zoom level, and other properties around what your map is
+         * displaying.
+         * Here, you created an update that points the camera at a specific
+         * LatLngBounds. You can think of a LatLngBounds as a rectangle
+         * around a set of points. You can make one explicitly by saving
+         * what the southwest and northeast corners of it should be.
+         * More often, it is easier to provide a list of points that
+         * you would like this rectangle to encompass.
+
+         */
+
+        int margin = getResources().getDimensionPixelSize(R.dimen.map_inset_margin);
+        CameraUpdate update = CameraUpdateFactory.newLatLngBounds(bounds, margin);
+        mMap.animateCamera(update);
     }
 
     private class SearchTask extends AsyncTask<Location, Void, Void> {
         private GalleryItem mGalleryItem;
         private Bitmap mBitmap;
+        private Location mLocation;
 
         @Override
         protected Void doInBackground(Location... locations) {
+            mLocation = locations[0];
             FlickrFetchr fetchr = new FlickrFetchr();
             List<GalleryItem> items = fetchr.searchPhotos(locations[0]);
 
@@ -216,8 +269,12 @@ public class LocatrFragment extends Fragment {
         }
 
         @Override
-        protected void onPostExecute(Void aVoid) {
-            mImageView.setImageBitmap(mBitmap);
+        protected void onPostExecute(Void result) {
+            mMapImage = mBitmap;
+            mMapItem = mGalleryItem;
+            mCurrentLocation = mLocation;
+
+            updateUI();
         }
     }
 }
